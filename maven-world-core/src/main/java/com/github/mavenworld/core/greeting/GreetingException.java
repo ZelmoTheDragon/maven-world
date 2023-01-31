603e6803da7aacbffb18790c38b3a93c8e5b0972
Maxime MOSELLE <maximemoselle@gmail.com>
2023-01-31 21:35:00 +0100

    Ajout configuration du compilateur.

--- maven-world-core/src/main/java/com/github/mavenworld/core/greeting/GreetingException.java
+++ maven-world-core/src/main/java/com/github/mavenworld/core/greeting/GreetingException.java
@@ -1,6 +1,11 @@
 package com.github.mavenworld.core.greeting;
 
-class GreetingException extends RuntimeException{
+import java.io.Serial;
+
+class GreetingException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     GreetingException(String message) {
         super(message);
